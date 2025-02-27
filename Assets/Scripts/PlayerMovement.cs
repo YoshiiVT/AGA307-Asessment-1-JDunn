a44831df4f2e485f518166d77cf4fb862fe28d7e
YoshiiVT <YoshiiUkichiro@outlook.com>
2025-02-27 20:06:11 +1100

    Fixed Sprint, Added Projectile, Added Enemy and Killbuttons

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,9 @@ public class PlayerMovement : MonoBehaviour
 
     //Reference for speed controll
     public float speed = 6f;
-    public float walking = 6f;
-    public float sprinting = 12f;
-    public bool isSprinting;
+    private float pSpeed = 0f;
+    private bool isSprint = false;
+    private int multiplier = 2;
 
     //These references are our variables to simulate gravity
     Vector3 velocity;
@@ -25,8 +25,14 @@ public class PlayerMovement : MonoBehaviour
     //Jump Height
     public float jumpHeight = 3f;
 
+    private void Start()
+    {
+        pSpeed = speed;
+    }
+
     void Update()
     {
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         // GroundDistance is the radius of the sphere, the layermask is GroundMask
 
@@ -39,13 +45,14 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+       
         //Movement in a direction that our inputs go into
         Vector3 move = transform.right * x + transform.forward * z;
         //What is above tells the computer what direction we want to go by the way we are facing and what inputs we put in.
         // If we use "Vector3 move = new Vector3(x, 0f, z);" it will work moving us on global coordinates, but it wont move us in the direction we face.
         // However we still need to add a "motor" that moves us, which is the character controller
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * pSpeed * Time.deltaTime);
         //Controlle.Move is a prewritten instruction that moves a Vector 3 in the brackets
         //Remember "Time.deltaTime" makes a variable frame rate independant
 
@@ -55,32 +62,38 @@ public class PlayerMovement : MonoBehaviour
         }
         //More important physics shit, basically " V=Square Root (Jumpheight x -2 x G) "
 
-        //Check to see the sprint key is down and is grounded
-        if (Input.GetButton("Fire3") && isGrounded)
-        {
-            //Set speed to sprint speed
-            isSprinting = true;
-        }
-        else 
-        { 
-            //Set speed to walk speed
-            isSprinting = false; 
-        } 
-        
-        //Speed modifiers
-        if(isSprinting == true)
-        {
-            speed = sprinting;
-        }
-
-        if (isSprinting == false)
-        {
-            speed = walking;
-        }
-
         velocity.y += gravity * Time.deltaTime;
         //This is all just physics and math stuff that brendan can yell at me later...
         controller.Move(velocity * Time.deltaTime);
         //Velocity Formula " V = 1/2Gravity x Time(Squared) " 
+
+        if (Input.GetButton("Fire3"))
+        {
+            if (isGrounded == false)
+            { return; }
+            isSprint = true;
+            Sprinting();
+        }
+        else
+        {
+            if (isGrounded == false)
+            { return; }
+            isSprint = false;
+            Sprinting();
+        }
+    }
+
+    private void Sprinting()
+    {
+   
+        if (isSprint)
+        {
+            pSpeed = speed * multiplier;
+        }
+        else
+        {
+            pSpeed = speed;
+        }
+        
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject hitParticles;
+    /// <summary>
+    /// When script is run it will wait 3 seconds before it runs the destroy function
+    /// </summary>
+    private void Start()
+    {
+        Invoke("DestroyProjectile", 3);
+    }
+
+    /// <summary>
+    /// This is the function that will destroy the projectile
+    /// </summary>
+    public void DestroyProjectile()
+    {
+        Destroy(gameObject);
+        GameObject particles = Instantiate(hitParticles, transform.position, transform.rotation);
+    }
+
+    /// <summary>
+    /// This will activate the destroy function early if it collides with something
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            collision.gameObject.GetComponentInChildren<Renderer>().material.color = Color.red;
+            Destroy(collision.gameObject, 1);
+        }
+        DestroyProjectile();
+    }
+}
